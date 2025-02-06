4f2be27e5693f9a14941978135d1a59bbf689d34
Ruoneee <your-email@example.com>
2025-02-07 06:40:19 +0800

    With BG Music

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,131 +35,33 @@ class _GolfCupScreenState extends State<GolfCupScreen> {
   String _participantName = "-";
   String _holeNo = "-";
 
+  late final AudioPlayer _audioPlayer;
+
   final List<Map<String, String>> participants = [
     // HOLE 1
     {"partno": "1", "name": "RENE CUERPO", "hole": "HOLE 1 - GROUP A"},
     {"partno": "2", "name": "SONNY HERNANDEZ", "hole": "HOLE 1 - GROUP A"},
     {"partno": "3", "name": "POGI MARIANO", "hole": "HOLE 1 - GROUP A"},
     {"partno": "4", "name": "ERIC ILLESCAS", "hole": "HOLE 1 - GROUP A"},
-    {"partno": "1", "name": "HARRY PALTONGAN", "hole": "HOLE 1 - GROUP B"},
-    {"partno": "2", "name": "PENG ISTANISLAO", "hole": "HOLE 1 - GROUP B"},
-    {"partno": "3", "name": "VER BUCAT", "hole": "HOLE 1 - GROUP B"},
-    {"partno": "4", "name": "JOJO ALVAREZ", "hole": "HOLE 1 - GROUP B"},
-
-    // HOLE 2
-    {"partno": "1", "name": "REY GARCIA", "hole": "HOLE 2 - GROUP A"},
-    {"partno": "2", "name": "ALBERT LAI", "hole": "HOLE 2 - GROUP A"},
-    {"partno": "3", "name": "SIM APALISOK", "hole": "HOLE 2 - GROUP A"},
-    {"partno": "4", "name": "CEZAR CAL", "hole": "HOLE 2 - GROUP A"},
-    {"partno": "1", "name": "ED FRANCISCO", "hole": "HOLE 2 - GROUP B"},
-    {"partno": "2", "name": "MER RODRIGUEZ", "hole": "HOLE 2 - GROUP B"},
-    {"partno": "3", "name": "MILO LUCENARIO", "hole": "HOLE 2 - GROUP B"},
-    {"partno": "4", "name": "YARLIE LIM", "hole": "HOLE 2 - GROUP B"},
-
-    // HOLE 3
-    {"partno": "1", "name": "LAKAN FONACIER", "hole": "HOLE 3 - GROUP A"},
-    {"partno": "2", "name": "JUAN PAULO CANLAS", "hole": "HOLE 3 - GROUP A"},
-    {"partno": "3", "name": "AMIEL JUBILAN", "hole": "HOLE 3 - GROUP A"},
-    {"partno": "4", "name": "RAYMUND DALANGIN", "hole": "HOLE 3 - GROUP A"},
-    {"partno": "1", "name": "JOHNNY GUITERREZ", "hole": "HOLE 3 - GROUP B"},
-    {"partno": "2", "name": "ALEX SUELILA", "hole": "HOLE 3 - GROUP B"},
-    {"partno": "3", "name": "BENJO BERNARDO", "hole": "HOLE 3 - GROUP B"},
-    {"partno": "4", "name": "EGAY DA ROSAS", "hole": "HOLE 3 - GROUP B"},
-
-    // HOLE 4
-    {"partno": "1", "name": "JULIUS DRILON", "hole": "HOLE 4 - GROUP A"},
-    {"partno": "2", "name": "ROMEO REYES", "hole": "HOLE 4 - GROUP A"},
-    {"partno": "3", "name": "MICHAEL DRILON", "hole": "HOLE 4 - GROUP A"},
-    {"partno": "4", "name": "SAMANTHA DRILON", "hole": "HOLE 4 - GROUP A"},
-    {"partno": "1", "name": "BIANCO MARIANO", "hole": "HOLE 4 - GROUP B"},
-    {"partno": "2", "name": "ALJHEN SUELILA", "hole": "HOLE 4 - GROUP B"},
-    {"partno": "3", "name": "DIONE RAMOS", "hole": "HOLE 4 - GROUP B"},
-    {"partno": "4", "name": "CB ROARO", "hole": "HOLE 4 - GROUP B"},
-
-    // HOLE 5
-    {"partno": "1", "name": "CALOY REYES", "hole": "HOLE 5 - GROUP A"},
-    {"partno": "2", "name": "JIGGY GARCIA", "hole": "HOLE 5 - GROUP A"},
-    {"partno": "3", "name": "MANNY CABUNGCAL", "hole": "HOLE 5 - GROUP A"},
-    {"partno": "4", "name": "GERRY GONZALEZ", "hole": "HOLE 5 - GROUP A"},
-    {"partno": "1", "name": "ELMO SAN DIEGO SR.", "hole": "HOLE 5 - GROUP B"},
-    {"partno": "2", "name": "GEORGE REGIS", "hole": "HOLE 5 - GROUP B"},
-    {"partno": "3", "name": "JUN FERRER", "hole": "HOLE 5 - GROUP B"},
-    {"partno": "4", "name": "PINGOY LAGUMBAY", "hole": "HOLE 5 - GROUP B"},
-    {"partno": "1", "name": "ALEX PAPA", "hole": "HOLE 5 - GROUP C"},
-    {"partno": "2", "name": "JULES CHANCO", "hole": "HOLE 5 - GROUP C"},
-    {"partno": "3", "name": "HECTOR MAGPANTAY", "hole": "HOLE 5 - GROUP C"},
-    {"partno": "4", "name": "TOTI ISIP", "hole": "HOLE 5 - GROUP C"},
-
-    // HOLE 6
-    {"partno": "1", "name": "AYEE BEECH", "hole": "HOLE 6 - GROUP A"},
-    {"partno": "2", "name": "WILLIAM VALO", "hole": "HOLE 6 - GROUP A"},
-    {"partno": "3", "name": "CHRIS DAVIES", "hole": "HOLE 6 - GROUP A"},
-    {"partno": "4", "name": "GREG FIGUEROA", "hole": "HOLE 6 - GROUP A"},
-    {"partno": "1", "name": "ALEX GORNE", "hole": "HOLE 6 - GROUP B"},
-    {"partno": "2", "name": "OGENG PALASIGUE", "hole": "HOLE 6 - GROUP B"},
-    {"partno": "3", "name": "DING MAULEON", "hole": "HOLE 6 - GROUP B"},
-    {"partno": "4", "name": "EDDIE CHEANG", "hole": "HOLE 6 - GROUP B"},
-
-    // HOLE 7
-    {"partno": "1", "name": "AGNES ILLESCAS", "hole": "HOLE 7 - GROUP A"},
-    {"partno": "2", "name": "STEPHANIE MELENDRES", "hole": "HOLE 7 - GROUP A"},
-    {"partno": "3", "name": "JOY MEGUITO", "hole": "HOLE 7 - GROUP A"},
-    {"partno": "4", "name": "NEIL MENGUITO", "hole": "HOLE 7 - GROUP A"},
-    {"partno": "1", "name": "DAN TOLENTINO", "hole": "HOLE 7 - GROUP B"},
-    {"partno": "2", "name": "DONG GO", "hole": "HOLE 7 - GROUP B"},
-    {"partno": "3", "name": "JUN LANDAYAN", "hole": "HOLE 7 - GROUP B"},
-    {"partno": "4", "name": "TONY CALVELO", "hole": "HOLE 7 - GROUP B"},
-
-    // HOLE 8
-    {"partno": "1", "name": "RAMONY SY", "hole": "HOLE 8 - GROUP A"},
-    {"partno": "2", "name": "OLIVER SALUD", "hole": "HOLE 8 - GROUP A"},
-    {"partno": "3", "name": "PATRICK BAGSIK", "hole": "HOLE 8 - GROUP A"},
-    {"partno": "4", "name": "PORFIRIO BAGSIK JR.", "hole": "HOLE 8 - GROUP A"},
-    {"partno": "1", "name": "ANDY HIRANG", "hole": "HOLE 8 - GROUP B"},
-    {"partno": "2", "name": "EDDIE COLLADO", "hole": "HOLE 8 - GROUP B"},
-    {"partno": "3", "name": "DAN JAVIER", "hole": "HOLE 8 - GROUP B"},
-    {"partno": "4", "name": "SUY MINIGAWA", "hole": "HOLE 8 - GROUP B"},
-
-    // HOLE 9
-    {"partno": "1", "name": "LEE LONGA", "hole": "HOLE 9 - GROUP A"},
-    {"partno": "2", "name": "WINSTON GINEZ", "hole": "HOLE 9 - GROUP A"},
-    {"partno": "3", "name": "ALVIN JADE RIVANO", "hole": "HOLE 9 - GROUP A"},
-    {"partno": "4", "name": "REMAUR MIRO", "hole": "HOLE 9 - GROUP A"},
-    {"partno": "1", "name": "MAI GONZALEZ", "hole": "HOLE 9 - GROUP B"},
-    {"partno": "2", "name": "BERNARD JAO", "hole": "HOLE 9 - GROUP B"},
-    {"partno": "3", "name": "NOEL ISIP", "hole": "HOLE 9 - GROUP B"},
-    {"partno": "4", "name": "MARTIN CASTRO", "hole": "HOLE 9 - GROUP B"},
-
-    // HOLE 10
-    {"partno": "1", "name": "RAMIR ENCABO", "hole": "HOLE 10 - GROUP A"},
-    {"partno": "2", "name": "ABEL PANGAN", "hole": "HOLE 10 - GROUP A"},
-    {"partno": "3", "name": "MERK LIMBO", "hole": "HOLE 10 - GROUP A"},
-    {"partno": "4", "name": "ARON FRANCISCO", "hole": "HOLE 10 - GROUP A"},
-    {"partno": "1", "name": "MAX CAVEL", "hole": "HOLE 10 - GROUP B"},
-    {"partno": "2", "name": "IKE YUSINGCO", "hole": "HOLE 10 - GROUP B"},
-    {"partno": "3", "name": "MEL LATINAZO", "hole": "HOLE 10 - GROUP B"},
-    {"partno": "4", "name": "GERRY GUERRERO", "hole": "HOLE 10 - GROUP B"},
-
-    // HOLE 11
-    {"partno": "1", "name": "DAN YATCO", "hole": "HOLE 11 - GROUP A"},
-    {"partno": "2", "name": "ARNEL LAGAREJOS", "hole": "HOLE 11 - GROUP A"},
-    {"partno": "3", "name": "ARNEL ESTACIO", "hole": "HOLE 11 - GROUP A"},
-    {"partno": "4", "name": "IRENE DEL ROSARIO", "hole": "HOLE 11 - GROUP A"},
-    {"partno": "1", "name": "ELMO SAN DIEGO JR.", "hole": "HOLE 11 - GROUP B"},
-    {"partno": "2", "name": "JAMES REYES", "hole": "HOLE 11 - GROUP B"},
-    {"partno": "3", "name": "MON ISIP", "hole": "HOLE 11 - GROUP B"},
-    {"partno": "4", "name": "CHARLIE ISIP", "hole": "HOLE 11 - GROUP B"},
-
-    // HOLE 12
-    {"partno": "1", "name": "DAN CUERPO", "hole": "HOLE 12 - GROUP A"},
-    {"partno": "2", "name": "JOEL PINEDA", "hole": "HOLE 12 - GROUP A"},
-    {"partno": "3", "name": "RAMIR TIAMZON", "hole": "HOLE 12 - GROUP A"},
-    {"partno": "4", "name": "DINO CREZO", "hole": "HOLE 12 - GROUP A"},
-    {"partno": "1", "name": "PAULO AGUILAR", "hole": "HOLE 12 - GROUP B"},
-    {"partno": "2", "name": "MIKE ANTONIO", "hole": "HOLE 12 - GROUP B"},
-    {"partno": "3", "name": "BONA MORALES", "hole": "HOLE 12 - GROUP B"},
-    {"partno": "4", "name": "PAUL REYES", "hole": "HOLE 12 - GROUP B"},
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _playBackgroundMusic();
+  }
+
+  Future<void> _playBackgroundMusic() async {
+    await _audioPlayer.setReleaseMode(ReleaseMode.loop); // Keep looping the music
+    await _audioPlayer.play(AssetSource('background.mp3'));
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   void _searchParticipant() {
     String searchName = _searchController.text.toUpperCase();
@@ -184,7 +87,8 @@ class _GolfCupScreenState extends State<GolfCupScreen> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text("Participant Not Found"),
-          content: const Text("The name you entered is not in the list. Please type your full name correctly."),
+          content: const Text(
+              "The name you entered is not in the list. Please type your full name correctly."),
           actions: [
             TextButton(
               onPressed: () {
@@ -243,11 +147,11 @@ class _GolfCupScreenState extends State<GolfCupScreen> {
                     ),
                     child: Column(
                       children: [
-                        Image.asset('assets/banner.png', height: screenHeight * 0.25),
+                        Image.asset('assets/banner.png',
+                            height: screenHeight * 0.25),
                       ],
                     ),
                   ),
-
                   SizedBox(height: screenHeight * 0.015), // Closer spacing
 
                   // GREEN CONTAINER FOR SEARCH & RESULTS
